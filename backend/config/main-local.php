80ef9fa2fedf62a463bcbcce5f93545895ae8829
trung <trungduc.vnu@gmail.com>
2021-11-09 17:36:36 +0700

    up

---
+++ backend/config/main-local.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'orrrLDh9HrOEH2y8f97Yj71NBUiDJJp7',
+        ],
+        // 'urlManagerFrontEnd' => [
+        //     'class' => 'yii\web\urlManager',
+        //     'baseUrl' => '/gcaeco',
+        //     'enablePrettyUrl' => true,
+        //     'showScriptName' => false,
+        // ],
+    ],
+];
